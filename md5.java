082fdc219311205b6177fc8e742722d9fa754a0b
Rachel Gao <rachelgao0421@gmail.com>
2024-05-23 15:05:19 -0400

    made skeelton

--- md5.java
+++ md5.java
@@ -1,1 +1,17 @@
+public class md5{
+    public static void main(String args[]){
+        
+        if (args.length() < 1) System.out.println("no mode specified");
+        else{
+            if (args[0].compareTo("encode") == 0) encode(args[1]);
+            //if (args[0].compareTo("decode") == 0) decode(args[1]);
+            else System.out.println("mode doesnt exist");
+        }
 
+    }
+
+    public static void encode(String line){
+        
+    }
+
+}
