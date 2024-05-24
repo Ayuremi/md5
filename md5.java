d6181288631b8c2e9fb18446c8dbdbbc8d745434
Nathaniel Ciu <nathanielciu@Nathaniels-MacBook-Air.local>
2024-05-24 14:40:04 -0400

    Got Binary String Print to work

--- md5.java
+++ md5.java
@@ -1,16 +1,37 @@
+import java.nio.charset.StandardCharsets;
+
 public class md5{
     public static void main(String args[]){
-        
-        if (args.length() < 1) System.out.println("no mode specified");
+        // System.out.println("Main is called");
+
+        if (args.length < 1) System.out.println("no mode specified");
         else{
             if (args[0].compareTo("encode") == 0) encode(args[1]);
-            //if (args[0].compareTo("decode") == 0) decode(args[1]);
+            else if (args[0].compareTo("decode") == 0) decode(args[1]);
+            else if (args[0].compareTo("test") == 0) {printBinary(args[1]);}
             else System.out.println("mode doesnt exist");
         }
 
     }
 
+    public static void printBinary(String line) {
+        String holder = "";
+        byte[] lineBytes = line.getBytes(StandardCharsets.US_ASCII);
+
+        for (int index = 0; index < lineBytes.length; index++) {
+            String byteString = Integer.toBinaryString(lineBytes[index]);
+            holder +=  String.format("%8s", byteString).replace(' ', '0') + "\n";
+        }
+
+        System.out.println(holder);
+
+    }
+
     public static void encode(String line){
+        
+    }
+
+    public static void decode(String line) {
         
     }
 
