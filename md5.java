40d2bb363368399b5a3dc215f2fbaa573529918d
Nathaniel Ciu <nathanielciu@Nathaniels-MacBook-Air.local>
2024-05-26 05:41:08 -0400

    added printArray function for debugging

--- md5.java
+++ md5.java
@@ -4,6 +4,7 @@ public class md5{
     public static void main(String args[]){
         // System.out.println("Main is called");
 
+
         if (args.length < 1) System.out.println("no mode specified");
         else{
             if (args[0].compareTo("encode") == 0) encode(args[1]);
@@ -12,15 +13,17 @@ public class md5{
             else System.out.println("mode doesnt exist");
         }
 
+
     }
 
     public static void printBinary(String line) {
         String holder = "";
         byte[] lineBytes = line.getBytes(StandardCharsets.US_ASCII);
 
+
         for (int index = 0; index < lineBytes.length; index++) {
             String byteString = Integer.toBinaryString(lineBytes[index]);
-            holder +=  String.format("%8s", byteString).replace(' ', '0') + "\n";
+            holder +=  String.format("%8s", byteString).replace(' ', '0') + " ";
         }
 
         System.out.println(holder);
@@ -30,15 +33,27 @@ public class md5{
         
     }
 
-    public static void paddingPassword() {
+    public static void paddingPassword(String line) {
         // calculating how much padding is needed
-
+        
+        // 
     }
-
 
 
     public static void decode(String line) {
         
+    }
+
+    //helper functions 
+    public static void printByteArray(byte[] array) {
+        String holder = "";
+
+        for (int index = 0; index < array.length; index++) {
+            holder += array[index] + ", ";
+        }
+
+        System.out.println(holder); 
+
     }
 
 }
